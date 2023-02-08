4a2a78ca4be8ff8fb31bcbcbfcd54be2eb2d570a
lawrencechangit <118051543+lawrencechangit@users.noreply.github.com>
2023-02-08 12:36:10 +0800

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -17,7 +17,7 @@ def pin_to_ipfs(data):
 	projectSecret = "50ea124b3ea0cbb760fae468e2554c5e"
 	response = requests.post('https://ipfs.infura.io:5001/api/v0/add', files=files, auth=(projectId,projectSecret))
 	cid=response.text
-	hash_string=cid[2]
+	hash_string=cid[23:68]
 	print(hash_string)
 	#hash=cid.get("Hash")
 	#print("cid is ", hash)
