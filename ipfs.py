9b365a1f066d3501a92d3dc63b4531976c653531
lawrencechangit <118051543+lawrencechangit@users.noreply.github.com>
2023-02-08 11:04:51 +0800

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -4,8 +4,8 @@ import json
 def pin_to_ipfs(data):
 	assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
 	#YOUR CODE HERE
-	projectId = "<API_KEY>"
-	projectSecret = "<API_KEY_SECRET>"
+	projectId = "4948f0dbf13c4f82993af603b0ef4329"
+	projectSecret = "7e395492a0db48f4a0d7f8244902edd7"
 	response = requests.post('https://ipfs.infura.io:5001/api/v0/add', files=data, auth=(projectId,projectSecret))
 	cid=response
 	return cid
