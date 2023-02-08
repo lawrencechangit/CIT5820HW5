b2cb9ef89203f1c1590f04c3cc6be9caa13c7f97
lawrencechangit <118051543+lawrencechangit@users.noreply.github.com>
2023-02-08 13:30:14 +0800

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -22,7 +22,13 @@ def pin_to_ipfs(data):
 def get_from_ipfs(cid,content_type="json"):
 	assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
 	#YOUR CODE HERE	
-	print('CID is ', cid)
+	params = (
+	('arg','cid'),
+	)
+	projectId = "2LRLd1HNXgtfnG9eQ5OkDYy1Lqw"
+	projectSecret = "50ea124b3ea0cbb760fae468e2554c5e"
+	
+	response = requests.post('https://ipfs.infura.io:5001/api/v0/cat', params=params, auth=(projectId,projectSecret))
 	
 	assert isinstance(data,dict), f"get_from_ipfs should return a dict"
 	return data
