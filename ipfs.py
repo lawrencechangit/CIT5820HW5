460ba7e15ddb9a6a9ce9ff7d219b39a4901b66aa
lawrencechangit <118051543+lawrencechangit@users.noreply.github.com>
2023-02-08 11:59:45 +0800

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -13,9 +13,9 @@ def pin_to_ipfs(data):
     		'file': 'json_object'
 	}
 	
-	#projectId = "4948f0dbf13c4f82993af603b0ef4329"
-	#projectSecret = "7e395492a0db48f4a0d7f8244902edd7"
-	response = requests.post('https://ipfs.infura.io:5001/api/v0/add', files=files, auth=(4948f0dbf13c4f82993af603b0ef4329,7e395492a0db48f4a0d7f8244902edd7))
+	projectId = "4948f0dbf13c4f82993af603b0ef4329"
+	projectSecret = "7e395492a0db48f4a0d7f8244902edd7"
+	response = requests.post('https://ipfs.infura.io:5001/api/v0/add', files=files, auth=(projectId,projectSecret))
 	cid=response
 	print("cid is ", cid)
 	return cid
