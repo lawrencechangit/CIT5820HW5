ca940c3e3def5f21d8317de56c1e75cd185b2e22
lawrencechangit <118051543+lawrencechangit@users.noreply.github.com>
2023-02-08 13:26:42 +0800

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -22,7 +22,7 @@ def pin_to_ipfs(data):
 def get_from_ipfs(cid,content_type="json"):
 	assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
 	#YOUR CODE HERE	
-	print('CID is ", cid)
+	print('CID is ', cid)
 	
 	assert isinstance(data,dict), f"get_from_ipfs should return a dict"
 	return data
