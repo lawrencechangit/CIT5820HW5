3066a68113e88175e9acf4106754a9e3a11282b4
lawrencechangit <118051543+lawrencechangit@users.noreply.github.com>
2023-02-08 13:36:48 +0800

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -29,7 +29,8 @@ def get_from_ipfs(cid,content_type="json"):
 	projectSecret = "50ea124b3ea0cbb760fae468e2554c5e"
 	
 	response = requests.post('https://ipfs.infura.io:5001/api/v0/cat', params=params, auth=(projectId,projectSecret))
-	data=response.text
+	json_object=response.text
+	data = json.loads(json_object)
 	print(data)
 	
 	assert isinstance(data,dict), f"get_from_ipfs should return a dict"
