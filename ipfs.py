b40e67232b6336eef534d29960d480a0e92be2f7
lawrencechangit <118051543+lawrencechangit@users.noreply.github.com>
2023-02-09 21:16:13 +0800

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -29,6 +29,7 @@ def get_from_ipfs(cid,content_type="json"):
 	projectSecret = "50ea124b3ea0cbb760fae468e2554c5e"
 	
 	response = requests.post('https://ipfs.infura.io:5001/api/v0/cat', params=params, auth=(projectId,projectSecret))
+	print(response)
 	print(response.text)
 	data=response.text
 	#print(data)
